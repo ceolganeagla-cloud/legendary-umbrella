b501c3e1700a7a02d76f14ff5fc6e5edbcee2719
ceolganeagla-cloud <ceolganeagla@gmail.com>
2025-09-20 13:58:54 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,39 +1,8 @@
-const CACHE = 'cge-weather-v11';
-const CORE = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icon-192.png',
-  './icon-512.png',
-  './favicon.ico'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)));
-  self.skipWaiting();
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open("weather-v1").then(c => c.addAll([
+    "/", "/index.html", "/manifest.webmanifest", "/icon-192.png", "/icon-512.png"
+  ])));
 });
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  const req = e.request;
-  if (req.mode === 'navigate' ||
-      (req.method === 'GET' && req.headers.get('accept')?.includes('text/html'))) {
-    e.respondWith(fetch(req).then(r => {
-      const copy = r.clone(); caches.open(CACHE).then(c => c.put(req, copy)); return r;
-    }).catch(() => caches.match(req)));
-  } else {
-    e.respondWith(
-      caches.match(req).then(cached => cached || fetch(req).then(r => {
-        const copy = r.clone(); caches.open(CACHE).then(c => c.put(req, copy)); return r;
-      }))
-    );
-  }
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
